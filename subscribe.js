b21577e6df65b4f153e795675ba31a10ebd3c5c6
Daotama <daobrendan7@gmail.com>
2025-05-22 18:46:08 -0400

    Enhanced subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const axios = require('axios');
 
+const TWITCH_API_BASE = 'https://api.twitch.tv/helix/eventsub/subscriptions';
+
 async function getAccessToken() {
     const res = await axios.post('https://id.twitch.tv/oauth2/token', null, {
         params: {
@@ -12,6 +14,25 @@ async function getAccessToken() {
     return res.data.access_token;
 }
 
+async function listSubscriptions(token) {
+    const res = await axios.get(TWITCH_API_BASE, {
+        headers: {
+            'Client-ID': process.env.TWITCH_CLIENT_ID,
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    return res.data.data;
+}
+
+async function deleteSubscription(id, token) {
+    await axios.delete(`${TWITCH_API_BASE}?id=${id}`, {
+        headers: {
+            'Client-ID': process.env.TWITCH_CLIENT_ID,
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    console.log(`Deleted existing subscription: ${id}`);
+}
 async function createSubscription(token) {
     const payload = {
         type: 'channel.channel_points_custom_reward_redemption.add',
@@ -26,7 +47,7 @@ async function createSubscription(token) {
         }
     };
 
-    const res = await axios.post('https://api.twitch.tv/helix/eventsub/subscriptions', payload, {
+    const res = await axios.post(TWITCH_API_BASE, payload, {
         headers: {
             'Client-ID': process.env.TWITCH_CLIENT_ID,
             'Authorization': `Bearer ${token}`,
@@ -40,6 +61,16 @@ async function createSubscription(token) {
 (async () => {
     try {
         const token = await getAccessToken();
+        const subscriptions = await listSubscriptions(token);
+
+        for (const sub of subscriptions) {
+            if (
+                sub.type === 'channel.channel_points_custom_reward_redemption.add' &&
+                sub.condition.broadcaster_user_id === process.env.BROADCASTER_ID
+            ) {
+                await deleteSubscription(sub.id, token);
+            }
+        }
         await createSubscription(token);
     } catch (error) {
         console.error('Failed to subscribe:', err.response?.data || err.message);
